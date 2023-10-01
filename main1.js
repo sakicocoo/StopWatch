7d0690c2e3efc9ad5f9ec4ffdd8c89ce374fb4f3
EC2 Default User <ec2-user@ip-172-31-8-255.ap-northeast-1.compute.internal>
2023-10-01 02:04:59 +0000

    ストップウォッチ課題提出

--- main1.js
+++ main1.js
@@ -1,63 +1,32 @@
-const startButton = document.getElementById('start');
-const stopButton = document.getElementById('stop');
-const resetButton = document.getElementById('reset');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
 
-const time = document.getElementById('time')
+const showTime = document.getElementById('time');
 
-// msec = 0;
-// sec=0;
-// min=0;
-// hour=0;
-
-// function timer(){
-//   msec += 1;
-//   if (msec > 99) {
-//     mesc = 0;
-//     sec += 1;
-//   }
-//   if (sec > 59) {
-//     sec=0;
-//     min+=1;
-//   }
-//   if (min > 59) {
-//     min=0;
-//     hour+=1;
-//   }
-//   if (hour > 99){
-//     msec=0;
-//     sec=0;
-//     min=0;
-//     hour=0;
-//   }
-// }
-
+let timer;
 let startTime;
-let showTime;
-let stopTime;
 let elapsedTime = 0;
 let holdTime = 0;
 
 function displayTime() {
-  const currentTime = new Date(Date.now() - startTime + stopTime);
-  const h = String(currentTime.getHours()-1).padStart(2, '0');
-  const m = String(currentTime.getMinutes()).padStart(2, '0');
-  const s = String(currentTime.getSeconds()).padStart(2, '0');
-  const ms = String(currentTime.getMilliseconds()).padStart(2, '0');
-
-  time.textContent = `${h}:${m}:${s}.${ms}`;
-  showTime = setTimeout(displayTime, 10);
+  timer= setTimeout(function() {
+    elapsedTime=Date.now()- startTime+holdTime;
+    showTime.textContent = new Date(elapsedTime).toISOString().slice(14, 22);
+    displayTime();
+  },10);
 }
 
 //Startボタン押下
 //タイマーが動く
 // startボタンが押せなくなる
 // stop,resetボタンがおせる
-startButton.addEventListener('click', function() {
-  startTime = Date.now()
-  displayTime()
-  startButton.disabled = true;
-  stopButton.disabled = false;
-  resetButton.disabled = false;
+startBtn.addEventListener('click', function() {
+  startTime = Date.now();
+  displayTime();
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  resetBtn.disabled = false;
 });
 
 // Stopボタン押下
@@ -65,7 +34,25 @@ startButton.addEventListener('click', function() {
 // Stopボタンが押せなくなる
 // Start,resetボタンがおせる
 
+stopBtn.addEventListener('click', function() {
+  clearInterval(timer);
+  holdTime += Date.now() - startTime;
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  resetBtn.disabled = false;
+});
+
 // Resetボタン押下
 // タイマーがゼロになる
 // Reset、Stopが押せなくなる
 // Startボタンが押せる
+
+resetBtn.addEventListener('click', function() {
+  clearInterval(timer);
+  elapsedTime=0;
+  holdTime=0;
+  showTime.textContent="00:00:00";
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  resetBtn.disabled = true;
+});
